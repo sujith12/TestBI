41b84249dd3f064414206a0d78215ce2cccd0a44
Sujith Koganti <sujith.koganti@Nebraska.gov>
2018-09-10 16:08:40 -0500

    Setting up the project

---
+++ LocalRepo/App_Code/TestClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for TestClass
+/// </summary>
+public class TestClass
+{
+    public TestClass()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+
+    public class CreatePullRequest
+    {
+        //Create pull request class
+    }
+    public class TestGitClass
+    {
+        public TestGitClass()
+        {
+            Console.WriteLine("GIT");
+        }
+        //commit for class
+
+        //nothing to build
+       public void TestMethod()
+        { //testing
+            Console.WriteLine("testing application");
+            Console.ReadLine();
+        }
+        //commit for clas
+        //testing git and github
+    }
+}
